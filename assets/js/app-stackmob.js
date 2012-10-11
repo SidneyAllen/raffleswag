d67bf87ad49459fbdf75dd2c0c4e5b1a3ec3751d
Sidney Maestre <sid@crushitmobile.com>
2012-10-10 17:20:29 -0700

    update verify page

--- assets/js/app-stackmob.js
+++ assets/js/app-stackmob.js
@@ -198,7 +198,7 @@ StackMob.init({
 		    e.preventDefault();
 		    console.log(app.mobile);
 		    console.log(model.toJSON());
-		    app.prize_id = model.prize_id
+		    app.prize_id = model.get("prize_id");
 		    router.navigate('#checkin',{trigger: true, replace: false})
 			console.log('get')
 			/*
